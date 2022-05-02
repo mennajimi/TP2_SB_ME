aa08c5f3a91ac349da71fca8cb225f01259b2b15
mennajimi <myriam.ennajimi@hotmail.com>
2022-05-02 01:27:28 -0400

    Voire commentaire au ligne suivante Ligne 64 de RunProg.py Ligne 41 app.py Choses a faire: Pour les paramètre, il faut les afficher depuis le dictionnaire quand on met a jour la page, i.e quand on clique sur execute.
    Tu clique sur validate parameters pour valider l les paramètres et e fichier arbre et ca fait apparaitre le bouton execute. Si tu cliques sur submit tree, ca ne mene nul part, donc sauras un message d'erreur parce que la fonction submit Tree nexiste plus(est en commentaire)
    Le programme roule
    Donc il faut la page pour le statut d'exécution, la mise a jour automatique
    
    bouton réinitialiser etc.

--- app/app.py
+++ app/app.py
@@ -8,10 +8,12 @@ __auteur__ = "Alix Boc"
 
 import urllib.request, json
 from flask import Flask, render_template, request, redirect, url_for, jsonify
-import bd, datetime, os
+import bd, os
+from datetime import datetime
 #from flask_session import Session
 #from validation import Validation
-from validation_Field import Validation
+from validation import Validation
+# import seqgen_commands_web_field
 from RunProg import RunProg
 import re
 
@@ -29,12 +31,14 @@ bd.init_db()  #pas d'utilisation de la bd pour l'instant
 
 
 model_options = ["HKY","F84","GTR","JTT","WAG","PAM","BLOSUM","MTREV","CPREV45","MTART","LG","GENERAL"]
-params_list = []
+params_list = {}
+infile = ""
 
 @app.route('/')
 def default():
+    print(params_list)
     return render_template('seqgen_home.html', params=params_list)
-
+#a chaque fois que tu fais un render tempplate pour seqgen-home, on renvoit status
 
 @app.route('/results')
 def results():
@@ -67,53 +71,78 @@ def importerParamsFichier ():
             print("params",params_list)
     return default()
 
-@app.route ('/tree-load', methods=['POST'])
-def importerFichierArbre ():
-    file = request.files["file"]
-    file.save(os.path.join(app.config['tmp'], file.filename))  # sauver le fichier dans le répertoire tmp
-    filename = app.config['tmp'] + "/" + file.filename  # faire un chemin
-    return default()
+# @app.route ('/submitTree', methods=['POST']) #TO DO
+# def submitTree ():
+    # global infile
+    # if request.form["optionTree"] == "pasted":
+    #     infile = request.form["treeEntry"]
+    # elif request.form["optionTree"] =="file":
+    #     infile = []
+    #     file = request.files["userfile"]
+    #     print(file.filename)
+    #     # file.save(os.path.join(app.config['tmp'], file.filename))  # sauver le fichier dans le répertoire tmp
+    #     infile = app.config['tmp'] + "/" + file.filename  # faire un chemin
+    #     #file_in = open(filename, "r")
+    #     #for ligne in file_in:
+    #     #    if not ligne.strip():
+    #     #        continue
+    #     #    else:
+    #      #       infile.append(ligne.strip("\n"))
+    #     #infile = str(infile)
+    #     #file_in.close()
+    # print(infile)
+    # return render_template('seqgen_home.html', tree=infile)
 
-@app.route ('/paramsField', methods=['POST']) #en dévelop.
-def importerParamsDuChamp ():
-    params_list = []
-    model = "-m"+str(model_options[int(request.form["model"])])
-    length = "-l"+str(request.form["length"])
-    i_range = "-i"+str(request.form["i_range"])
-    options = [model,length,i_range]
-    for i in options:
-        params_list.append(i)
+
+@app.route ('/paramsField', methods=['POST']) # Fini pour les paramètres de base :)
+def paramsField():
+    print("coucou")
+    # model = "-m"+str(model_options[int(request.form["model"])])
+    # length = "-l"+str(request.form["length"])
+    # i_range = "-i"+str(request.form["i_range"])
+
+    global params_list
+    params_list["m"] = str(request.form["model"])
+    params_list["l"] = str(request.form["length"])
+    params_list["i"] = str(request.form["i_range"])
+
     print(params_list)
-    V1 = Validation(params_list)
-    valide = V1.valide
-    print(valide)
-    if valide == False:
-        print("Params non valide")
-        return render_template('seqgen_home.html', erreur="Paramètres non valide, revoyez l'utilisation")
-    else:
-        return default()
 
-@app.route ('/validerParams',methods=['POST'])
-def validerParametres():
-    V1 = Validation(params_list, './tmp')
-    valide = V1.valide
-    print(valide)
-    if valide == False:
+    global infile
+    if request.form["optionTree"] == "pasted":
+        infile = request.form["treeEntry"]
+    elif request.form["optionTree"] == "file":
+        file = request.form["userfile"]
+        infile = app.config['tmp'] + "/" + file  # faire un chemin
+
+    print(infile)
+
+    if params_list["l"].isdigit() == False or params_list["m"] == "" or infile == "":
         print("Params non valide")
         return render_template('seqgen_home.html', erreur="Paramètres non valide, revoyez l'utilisation")
     else:
-        return default()
+        return render_template("seqgen_home.html", isValid=True)
+
+# @app.route ('/validerParams',methods=['POST'])
+# def validerParametres():
+#     V1 = Validation(params_list, './tmp')
+#     valide = V1.valide
+#     print(valide)
+#     if valide == False:
+#         print("Params non valide")
+#         return render_template('seqgen_home.html', erreur="Paramètres non valide, revoyez l'utilisation")
+#     else:
+#         return default()
 
 @app.route ('/runprog',methods=['POST'])
 def runprog():
-    V1 = Validation(params_list, './tmp')
-    valide = V1.valide
-    params = V1.params
-    infile = V1.infile
-    outfile = V1.outfile
-    R1 = RunProg(params, infile, outfile, valide, './tmp')
+    # V1 = Validation(params_list, './tmp')
+    # valide = V1.valide
+    # params = V1.params
+
+    outfile = datetime.now().strftime("%H%M%S") + '_output_seqgen'
+    R1 = RunProg(params_list, infile, outfile, './tmp')
     R1.run()
-    print(valide)
     return default()
 
 @app.route('/<path:path>')
@@ -124,4 +153,4 @@ def all_files(path):
         return default()
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True, port=5000)
+    app.run(host="0.0.0.0", debug=True, port=4999)
